6d1fb9968df7e7c6db1d07fe3f44375883c04d0b
Ole Skyggebjerg <ole@skyggebjerg.dk>
2025-12-09 00:56:59 +0100

    Refactor project configuration and update dependencies
    - Removed unnecessary FS library reference in c_cpp_properties.json
    - Added SPIFFS filesystem support in platformio.ini
    - Updated project checksum
    - Deleted obsolete object file from build directory
    - Adjusted library dependencies in c_cpp_properties.json for better organization

--- .pio/build/m5stack-atoms3/lib760/M5Utility/m5_utility/crypto/des.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/m5stack-atoms3/lib760/M5Utility/m5_utility/crypto/des.cpp.o: \
- .pio/libdeps/m5stack-atoms3/M5Utility/src/m5_utility/crypto/des.cpp \
- .pio/libdeps/m5stack-atoms3/M5Utility/src/m5_utility/crypto/des.hpp
--- .pio/build/m5stack-atoms3/libf47/M5GFX/lgfx/utility/lgfx_miniz.c.d
+++
@@ -1,2 +0,0 @@
-.pio/build/m5stack-atoms3/libf47/M5GFX/lgfx/utility/lgfx_miniz.c.o: \
- .pio/libdeps/m5stack-atoms3/M5GFX/src/lgfx/utility/lgfx_miniz.c
